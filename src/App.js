e8494d445d8e029b25636a020745dd02a08dc1d7
Wojciech Słomczyński <w.sl@o2.pl>
2018-08-24 13:20:27 +0200

    add Profile component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Profile from './Profile/Profile';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Profile />
       </div>
     );
   }
