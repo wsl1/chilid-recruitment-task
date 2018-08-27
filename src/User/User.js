efb8b709d693d11f4948551891d592a3d4c339e6
Wojciech Słomczyński <w.sl@o2.pl>
2018-08-27 19:28:36 +0200

    add like and follow functions

--- src/User/User.js
+++ src/User/User.js
@@ -4,11 +4,9 @@ import UserPhoto from './User.jpg'
 import mock from '../mock.json';
 
 class User extends Component {
-   componentDidMount() {
-       console.log(1);
-   }
 
   render() {
+    const {likes, followers, like, follow} = this.props;
     return (
       <div className="user">
         <div className="user__top">
@@ -16,30 +14,30 @@ class User extends Component {
                 <img src={UserPhoto} className="user__img" alt="user"/>
             </div>
             <div className="user__details">
-                <p className="user__detail">
-                    <p className="user__name">{mock.name} {mock.surname} <span className="user__hearth">h</span></p>
+                <div className="user__detail">
+                    <p className="user__name">{mock.name} {mock.surname} <span onClick={like} className="user__hearth">h</span></p>
                     <p className="user__address">{mock.city}, {mock.country}</p>
-                </p>
+                </div>
             </div>
-            <div class="user__share">s</div>
+            <div className="user__share">s</div>
         </div>
         <div className="user__bottom">
-            <div class="user__stats">
-                <p className="user__stat user__likes">{mock.stats.likes}</p>
+            <div className="user__stats">
+                <p className="user__stat user__likes">{likes}</p>
                 <p className="user__stat-description">Likes</p>
             </div>
             <div className="vertical-line"></div>
-            <div class="user__stats">
+            <div className="user__stats">
                 <p className="user__stat user__following">{mock.stats.following}</p>
                 <p className="user__stat-description">Following</p>    
             </div>
             <div className="vertical-line"></div>
-            <div class="user__stats">
-                <p className="user__stat user__followers">{mock.stats.followers}</p>
+            <div className="user__stats">
+                <p className="user__stat user__followers">{followers}</p>
                 <p className="user__stat-description">Followers</p>
             </div>
-            <div class="user__follow">
-                <span class="user__follow-text">FOLLOW</span>
+            <div onClick={follow} className="user__follow">
+                <span className="user__follow-text">FOLLOW</span>
             </div>
         </div>
         
