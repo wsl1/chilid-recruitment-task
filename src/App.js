188ac03e52cf9bb220100e746dd74ea5735beb1e
Wojciech Słomczyński <w.sl@o2.pl>
2018-09-18 18:13:55 +0200

    add 'small' prop

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App" style={{margin: '20px'}}>
-        <Profile />
+        <Profile small/>
       </div>
     );
   }
